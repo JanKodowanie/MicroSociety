5702407f806ec05451b221742c94d1d875826505
dobrowj1 <01142560@pw.edu.pl>
2022-01-16 00:43:32 +0100

    Gateway and file serving is working

--- AccountService/core/middleware.py
+++ AccountService/core/middleware.py
@@ -12,7 +12,7 @@ from utils.hash import Hash
 from fastapi.security import OAuth2PasswordBearer
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/user-management/login")
 
 
 class TokenManager:
