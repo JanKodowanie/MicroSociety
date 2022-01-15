100ab3e1c1d8c17e6fad07904b5bc6672aa44c0d
dobrowj1 <01142560@pw.edu.pl>
2022-01-15 22:49:09 +0100

    Broker in docker! Hurray!

--- utils/reset_dbs.sh
+++ utils/reset_dbs.sh
@@ -5,9 +5,16 @@ array=( AccountService BlogWriteService EmailService)
 for i in "${array[@]}"
 do
     cd $i
+    rm -r media
 	rm db.sqlite
     rm db.sqlite-shm
     rm db.sqlite-wal
     aerich upgrade
     cd ..
 done
+
+rm -rf mongo
+mkdir mongo
+
+rm -rf rabbit
+mkdir rabbit
